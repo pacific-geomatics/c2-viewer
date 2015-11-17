72a475ae9c5a57b066a227a4589a5b3901e95714
Denis <carriere.denis@gmail.com>
2015-11-16 22:46:51 -0500

    Tile server reading gpkg

--- web/views.py
+++ web/views.py
@@ -39,3 +39,34 @@ def tms(basemap, zoom, x, y, ext):
     # Success
     else:
         return send_file(tile, mimetype='image/png')
+
+
+@app.route("gpkg/<basemap>/<int:zoom>/<int:x>/<int:y><ext>", methods=['GET', 'POST'])
+def gpkg(basemap, zoom, x, y, ext):
+
+    # Convert to Google Maps compatible format
+    google_y = (2 ** zoom) - y - 1
+
+    # Tile information
+    info = {
+        'basemap': basemap,
+        'x': x,
+        'y': y,
+        'google_y': google_y,
+        'zoom': zoom,
+        'ext': ext
+    }
+    tile = "/data/gpkg/{basemap}.gpkg".format(**info)
+
+    # Validate User
+    key = request.args.get('api_key')
+    if key not in ['123']:
+        return jsonify({'message': 'Not Authorized - Invalid Token'}), 401
+
+    # Check if Tile Exists
+    if not os.path.exists(tile):
+        return jsonify({'message': 'Tile does not exists'}), 404
+
+    # Success
+    else:
+        return send_file(tile, mimetype='image/png')
