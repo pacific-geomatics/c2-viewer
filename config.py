6b163257c8b00a1a0673f483fdde06e4c54dba01
Denis <carriere.denis@gmail.com>
2015-11-20 22:50:30 -0500

    Add everyone's VALID emails
    @Ambayee @rps333 @sleepycat @YOWGEO

--- config.py
+++ config.py
@@ -4,4 +4,11 @@ DEBUG = True
 SECRET_KEY = '4hLAJ6ZM9I4fL3lTu-PNNgMI'
 CLIENT_ID = '318442113295-5glonlcmqp4qhf7ut8bvobpkhnsj578m.apps.googleusercontent.com'
 APPLICATION_NAME = 'c2-viewer'
-VALID_EMAILS = ['carriere.denis@gmail.com', 'john@pacgeo.com', 'rps333@gmail.com']
+VALID_EMAILS = [
+    'carriere.denis@gmail.com',
+    'john@pacgeo.com',
+    'rps333@gmail.com',
+    'mike@korora.ca',
+    'ambayee@gmail.com',
+    'patrick.irwin@gmail.com'
+]
