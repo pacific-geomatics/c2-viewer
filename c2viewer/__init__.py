c018ab6f7dab4e4c8af51a6def090e7aaaded68a
Denis <carriere.denis@gmail.com>
2015-11-22 02:33:31 -0500

    #24 Add info logging save to file

--- c2viewer/__init__.py
+++ c2viewer/__init__.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import logging
 from flask import Flask
+from logging.handlers import RotatingFileHandler
+
 app = Flask(__name__)
 app.config.from_object('config')
+
+# Add Logging
+# https://gist.github.com/ibeex/3257877
+handler = RotatingFileHandler('c2viewer.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+app.logger.addHandler(handler)
 
 import c2viewer.views
 
