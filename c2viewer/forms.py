5210e94e76661f5ba4538f0ccf4b4269a9c5dd6a
Mike Williamson <mike@korora.ca>
2015-11-24 12:44:17 -0500

    Use PasswordField type for password input
    PasswordField produces <input type="password"> so that input to that
    field is masked.

--- c2viewer/forms.py
+++ c2viewer/forms.py
@@ -2,9 +2,10 @@
 
 from flask_wtf import Form
 from wtforms import StringField
+from wtforms import PasswordField
 from wtforms.validators import DataRequired
 
 
 class MyForm(Form):
     email = StringField('email', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
