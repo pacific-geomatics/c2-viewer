12c86d7020289470162c9081ea72a08804f54b35
Denis Carriere <carriere.denis@gmail.com>
2016-01-02 14:08:41 -0500

    Nginx Init Configuration

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,12 @@
-sudo apt-get install -y python-pip gunicorn
+# Install
+sudo apt-get install -y python-pip gunicorn nginx
 sudo pip install -r requirements.txt
+
+# Stormpath Settings
 mkdir -p ~/.stormpath
 cp apiKey.properties ~/.stormpath/apiKey.properties
+
+# Nginx Settings
+sudo cp ./nginx/c2viewer.pacgeo.com /etc/nginx/sites-available/c2viewer.pacgeo.com
+sudo cp ./nginx/nginx.conf /etc/nginx/nginx.conf
+sudo ln -s /etc/nginx/sites-available/c2viewer.pacgeo.com /etc/nginx/sites-enabled/c2viewer.pacgeo.com
