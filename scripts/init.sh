ce12880d247a3b5828a31d0e26048ea97a24b3f7
Denis Carriere <carriere.denis@gmail.com>
2016-01-02 13:23:39 -0500

    Changes to init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,4 @@
-sudo apt-get install python-pip
+sudo apt-get install -y python-pip gunicorn
 sudo pip install -r requirements.txt
 mkdir -p ~/.stormpath
-cp ../apiKey.properties ~/.stormpath/apiKey.properties
+cp apiKey.properties ~/.stormpath/apiKey.properties
