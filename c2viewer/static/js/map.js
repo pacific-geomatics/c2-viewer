4e05e41f9379486ec9343a51f14503968d7a525e
Denis <carriere.denis@gmail.com>
2016-01-07 22:05:54 -0500

    Add Back controls

--- c2viewer/static/js/map.js
+++ c2viewer/static/js/map.js
@@ -51,21 +51,53 @@ map.on('style.load', function () {
     });
 });
 
-map.addControl(new mapboxgl.Navigation({
-  position: 'top-right'
-}));
-
-
 document.addEventListener('DOMContentLoaded', function(){
 
   var ccv = function(){
+    var zoomin_control = document.querySelector('#zoom_in');
+    var zoomout_control = document.querySelector('#zoom_out');
     var logout_control = document.querySelector('#logout');
+    var rotate_control = document.querySelector('#rotate');
+    var tilt_control = document.querySelector('#tilt');
     var imagery_control = document.querySelector('#imagery');
     var topo_control = document.querySelector('#topo');
     var streets_control = document.querySelector('#streets');
 
     logout_control.addEventListener('click', function(e){
       location.href = '/logout'
+    });
+
+    // Slowly Fly to Location
+    // https://www.mapbox.com/mapbox-gl-js/example/flyto-options/
+    zoomin_control.addEventListener('click', function(e){
+      console.log('Set Zoom In');
+      map.flyTo({
+        zoom: map.getZoom() + 1,
+        speed: 0.3
+      })
+    })
+
+    zoomout_control.addEventListener('click', function(e){
+      console.log('Set Zoom Out');
+      map.flyTo({
+        zoom: map.getZoom() - 1,
+        speed: 0.3
+      })
+    });
+
+    tilt_control.addEventListener('click', function(e){
+      console.log('Set Pitch');
+      var cycle = {0: 30, 30: 60, 60: 0}
+      map.setPitch(cycle[map.getPitch()])
+    });
+
+    rotate_control.addEventListener('click', function(e){
+      console.log('Set Bearing')
+      map.flyTo({
+        bearing: map.getBearing() + 90,
+        zoom: map.getZoom() + 0.01,
+        speed: 0.005,
+      })
     });
 
     // Basemaps
