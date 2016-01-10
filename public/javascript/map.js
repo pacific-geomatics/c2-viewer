4dd9d4c3f17340ec32330891a36b331f17656360
Denis Carriere <carriere.denis@gmail.com>
2016-01-10 03:32:23 -0500

    Add Browserify

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -1,18 +1,13 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicGFjZ2VvIiwiYSI6ImE2ZmE3YTQyNmRjNTVmYTAxMWE2YWZlNGFjZjMzZWVhIn0.wRU0txw3VIEOVtyc8PCYdQ'
 
-// Set bounds to New York, New York
-var bounds = [
-  [-74.44747924804688, 40.54198241319326], // Southwest coordinates
-  [-73.46282958984375, 40.93011520598305]  // Northeast coordinates
-];
+var styles = require('./styles');
 
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v8',
+  style: styles.imagery,
   hash: true,
-  center: [-74.0015, 40.7268],
-  zoom: 14,
-  maxBounds: bounds,
+  center: [-77.693, 8.155],
+  zoom: 15,
   attributionControl: false
 });
 
